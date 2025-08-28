64a57ce4ec368f1bf8b37349731db6db094caac7
Bablu kumar <bk596572@gmail.com>
2025-08-28 12:43:37 +0530

    Initial database setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const dbUrl = process.env.ATLASDB_URL;
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+// Connect DB
+connectDB(dbUrl);
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server is running on port ${PORT}`);
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
